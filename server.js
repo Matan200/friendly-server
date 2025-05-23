d0ec9405d2b0e72733faee991349a988fd8a01b1
Tanya Rehby <tanyar@checkpoint.com>
2025-05-23 12:02:34 +0300

    friday23

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 // הגדרת CORS לפני הנתיבים
 app.use(
   cors({
-    origin: "http://localhost:3000", // אפשר גם '*' אם אתה רוצה לאפשר לכל כתובת לגשת
+    // origin: "http://localhost:3000", // אפשר גם '*' אם אתה רוצה לאפשר לכל כתובת לגשת
+origin: process.env.CLIENT_URL || "*",
+
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -20,15 +22,27 @@ app.use("/api", postRoutes); // כל הנתיבים של הפוסטים יהיו
 app.use("/api/users", userRoutes);
 app.use("/api/events", eventsRoutes);
 // חיבור ל-MongoDB Atlas
-mongoose
-  .connect(
-    "mongodb+srv://tanyarehby:lBBKT6Hh6ZPDRXoq@cluster0.n1rvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Posts",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("Connected to MongoDB Atlas")) // עדכון ההודעה
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+// mongoose
+//   .connect(
+//     "mongodb+srv://tanyarehby:lBBKT6Hh6ZPDRXoq@cluster0.n1rvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Posts",
+//     { useNewUrlParser: true, useUnifiedTopology: true }
+//   )
+//   .then(() => console.log("Connected to MongoDB Atlas")) // עדכון ההודעה
+//   .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 // מאזין על פורט 4000
-app.listen(4000, () => {
-  console.log("Server is running on http://localhost:4000");
-});
+// app.listen(4000, () => {
+//   console.log("Server is running on http://localhost:4000");
+// });
