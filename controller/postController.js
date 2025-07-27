9427edf3e0706b8ed43a15cce7d3d8418802f52a
Matan200 <matanle1@ac.sce.ac.il>
2025-07-27 13:50:37 +0300

    filter3

--- controller/postController.js
+++ controller/postController.js
@@ -185,19 +185,19 @@ const getLikeOnPost = async (req, res) => {
 
 const getFilteredPosts = async (req, res) => {
   try {
-    const { city, school, minAge, maxAge, /*subject,*/ gender, userType } =
+    const { city, school, minAge, maxAge, /*subject,*/ gender} =
       req.query;
-    console.log("User type received:in try", userType);
+    // console.log("User type received:in try", userType);
     let posts = await Post.find({}).populate("editor").sort({ createdAt: -1 });
-    console.log("User type received:after popu", userType);
-    if (userType) {
-          console.log("User type received: inside if", userType);
-      posts = posts.filter(
-        (post) =>
-          post.editor?.userType &&
-          post.editor.userType.toLowerCase() === userType.toLowerCase()
-      );
-    }
+    // console.log("User type received:after popu", userType);
+    // if (userType) {
+    //       console.log("User type received: inside if", userType);
+    //   posts = posts.filter(
+    //     (post) =>
+    //       post.editor?.userType &&
+    //       post.editor.userType.toLowerCase() === userType.toLowerCase()
+    //   );
+    // }
     if (city) {
           console.log("User type received:insidt city", city);
       posts = posts.filter(
